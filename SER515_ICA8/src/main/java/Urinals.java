d225a720ae043326cb14439a59465019c813eda7
Tahir Pervez <tpervez@asu.edu>
2022-10-27 11:32:06 -0700

    added javadoc

--- SER515_ICA8/src/main/java/Urinals.java
+++ SER515_ICA8/src/main/java/Urinals.java
@@ -4,13 +4,27 @@ public class Urinals {
     public Urinals() {
         urinals = "";
     }
+
+    /**
+     * @param input the input string, could be read in from file or console
+     * @return true if valid input, false if not
+     *          valid:  1 <= length <= 20
+     *                  contains only 0s and 1s
+     *                  contains no cases of "11"
+     */
     public boolean readIn(String input) {
         if(input.isEmpty() || input.length() > 20) {
             return false;
         }
+        char previous = '0';
         for (char curr : input.toCharArray()) {
             if(curr != '0' && curr != '1') {
                 return false;
+            } else {
+                if(previous == '1' && curr == '1') {
+                    return false;
+                }
+                previous = curr;
             }
         }
         urinals = input;
